ad252ecd9f640cea77eb19c57a5e6e52cf60d104
ViolinSolo <i.violinsolo@gmail.com>
2017-06-20 20:29:50 +0800

    [linear regression] challenge demo complete

--- LinearRegression/mine_for_Challenge.py
+++ LinearRegression/mine_for_Challenge.py
@@ -2,6 +2,12 @@ import pandas as pd
 from sklearn import linear_model
 import matplotlib.pyplot as plt
 
+# read old data
+data_frame_old = pd.read_fwf('brain_body.txt')
+X_old = data_frame_old[['Brain']]
+y_old = data_frame_old[['Body']]
+
+# read new challenge data
 data_frame_new = pd.read_csv('challenge_dataset.txt', header=None, names=['x2', 'y2'])
 print data_frame_new
 # X = data_new['a'].values.reshape(-1, 1)
@@ -14,6 +20,7 @@ lin_reg_model = linear_model.LinearRegression()
 lin_reg_model.fit(X, y)
 
 plt.scatter(X, y, marker='x')
-plt.scatter(X, lin_reg_model.predict(X), marker='.')
+# plt.scatter(X_old, y_old, marker='o')
 plt.plot(X, lin_reg_model.predict(X))
+plt.scatter(X, lin_reg_model.predict(X), marker='o')
 plt.show()
