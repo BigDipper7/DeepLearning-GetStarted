a09b65cf319d58045cfd5e285b8600234ced4ba5
ViolinSolo <i.violinsolo@gmail.com>
2017-07-12 19:58:43 +0800

    做了y标签的统计与数字化

--- TFLearn-NLP-EmotionDetect/mine_for_Challenge.py
+++ TFLearn-NLP-EmotionDetect/mine_for_Challenge.py
@@ -15,24 +15,45 @@ X_title = data_frame[['title']]
 # print X_title[0:100]
 # print data_frame['title'][:100]
 print X_title.size  # 18625 total lines
+print Y_score_phrase['score_phrase']
 
 
-# clean and pre-process of data
+# clean and pre-process of data, only pre-process X data, using VocabularyProcessor
 vocabulary_processor = VocabularyProcessor(max_document_length=MAX_TITLE_SIZE)
 i_generator = vocabulary_processor.fit_transform(raw_documents=data_frame['title'])
 X_All_word_ids = np.array(list(i_generator))
 for i in X_All_word_ids[:100]:
     print i
 vocabulary_processor.save('vocabulary_processor.dict')
+print 'dictionary had saved!'
 
 # replace origin data frame to after word id mapping iterable object
 X_title = X_All_word_ids
+print 'X_title:'
+print X_title
+
+
+print 'Y_score_phrase: size:'+str(len(Y_score_phrase))
+# pre-process Y data
+labels = {}
+labels_statistics = {}
+Y_score_phrase_nums = []
+for y_item in Y_score_phrase['score_phrase']:
+    print y_item
+    if y_item not in labels:
+        labels[y_item] = len(labels)
+        labels_statistics[y_item] = 0
+    Y_score_phrase_nums.append(labels[y_item])
+    labels_statistics[y_item] += 1
+
+print 'labels: ', labels
+print 'label statistics: ', labels_statistics
 
 
 # split the data
-n_split_train = int(X_title.size*0.8)
+n_split_train = int(len(X_title)*0.8)
 n_split_validate = int(n_split_train*0.8)
-print "Split will be at index 0 - %d - %d - %d" % (n_split_validate, n_split_train, X_title.size)
+print "Split will be at index 0 - %d - %d - %d" % (n_split_validate, n_split_train, len(X_title))
 train_X = X_title[:n_split_validate]
 validate_X = X_title[n_split_validate:n_split_train]
 test_X = X_title[n_split_train:]
@@ -44,6 +65,9 @@ test_Y = Y_score_phrase[n_split_train:]
 
 # print train_X[0:100]
 # train_X = pad_sequences(train_X, maxlen=10, value=0.)
+# change the data
+# train_Y = to_categorical(train_Y, nb_classes=8)
+# print train_Y
 
 
 # define network
