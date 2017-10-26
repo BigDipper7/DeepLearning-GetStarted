dd2e504182ad80e7a350d89260ab6598cb4751e8
ViolinSolo <i.violinsolo@gmail.com>
2017-10-26 14:27:53 +0800

    [image] 更新了 cuhk-03.mat 的文件extract方法

--- TF-Persion-ReID/cvpr-2015-paper/cuhk03_dataset.py
+++ TF-Persion-ReID/cvpr-2015-paper/cuhk03_dataset.py
@@ -5,8 +5,33 @@ import cv2
 import random
 import sys
 
+
 def prepare_data(path):
+    """
+    To extract cuhk-03.mat file, extract all the images ndarray and save them in the directory
+    :param path: the cuhk-03.mat file path
+    :return: None
+    """
+
     f = h5py.File('%s/cuhk-03.mat' % path)
+    # ==============
+    # f['labeled'] : <HDF5 dataset "labeled": shape (1, 5), type "|O">
+    #
+    # f['labeled'][0] : array([<HDF5 object reference>, <HDF5 object reference>,
+    #                           <HDF5 object reference>, <HDF5 object reference>,
+    #                           <HDF5 object reference>], dtype=object)
+    #
+    # [f['labeled'][0][i] for i in xrange(len(f['labeled'][0]))] :
+    #                       [<HDF5 object reference>, <HDF5 object reference>,
+    #                           <HDF5 object reference>, <HDF5 object reference>,
+    #                           <HDF5 object reference>]
+    #
+    # m = [f['labeled'][0][i] for i in xrange(len(f['labeled'][0]))]
+    # m[0] : <HDF5 object reference>
+    # f[m[0]] : <HDF5 dataset "b": shape (10, 843), type "|O">
+    # [f[m[0]][i] for i in xrange(len(f[m[0]]))] : shape(10, 843) 2-dims array
+    # ==============
+
     labeled = [f['labeled'][0][i] for i in xrange(len(f['labeled'][0]))]
     labeled = [f[labeled[0]][i] for i in xrange(len(f[labeled[0]]))]
     detected = [f['detected'][0][i] for i in xrange(len(f['detected'][0]))]
@@ -14,16 +39,20 @@ def prepare_data(path):
     datasets = [['labeled', labeled], ['detected', detected]]
     prev_id = 0
 
-    for dataset in datasets:
+    for dataset in datasets:  # example : dataset : ['labeled', labeled]
         if not os.path.exists('%s/%s/train' % (path, dataset[0])):
             os.makedirs('%s/%s/train' % (path, dataset[0]))
         if not os.path.exists('%s/%s/val' % (path, dataset[0])):
             os.makedirs('%s/%s/val' % (path, dataset[0]))
 
-        for i in xrange(0, len(dataset[1])):
+        for i in xrange(0, len(dataset[1])):  # dataset[1] shape(10, 843)
             for j in xrange(len(dataset[1][0])):
                 try:
+                    # dataset[1] shape(10, 843); 0<i<10, 0<j<843
+                    # dataset[1][i][j] shape(3-RGB-CHANNEL, WIDTH, HEIGHT) :
+                    #       example{<HDF5 dataset "Pn": shape (3, 93, 295), type "|u1">}
                     image = np.array(f[dataset[1][i][j]]).transpose((2, 1, 0))
+                    # image shape(HEIGHT, WIDTH, 3-RGB-CHANNEL)
                     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
                     image = cv2.imencode('.jpg', image)[1].tostring()
                     if len(dataset[1][0]) - j <= 100:
@@ -33,6 +62,7 @@ def prepare_data(path):
                         prev_id = j
                     with open(filepath, 'wb') as image_file:
                         image_file.write(image)
+                        print "Save (i:%d, j:%d) Image Success" % (i, j)
                 except Exception as e:
                     continue
 
