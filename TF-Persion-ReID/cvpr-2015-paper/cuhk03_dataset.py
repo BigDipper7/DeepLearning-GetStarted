08485e5f4ced0693d918f9d8255c063ad95b6bcf
ViolinSolo <i.violinsolo@gmail.com>
2017-11-02 01:53:05 +0800

    [image] all done

--- TF-Persion-ReID/cvpr-2015-paper/cuhk03_dataset.py
+++ TF-Persion-ReID/cvpr-2015-paper/cuhk03_dataset.py
@@ -66,7 +66,22 @@ def prepare_data(path):
                 except Exception as e:
                     continue
 
+
 def get_pair(path, set, num_id, positive):
+    """
+    get a pair of positive or negative example, Must be a pair:
+        1. randomly get/generate a random (id, id) pair between [0, num_id],
+            if positive, get an pair with same [id], means two image from the same entity, only has diff posture
+            if negative, get an pair with diff [ids], means two image from two entities, diff category
+        2. randomly generate an entity instance between [0, 10] with the specified id: [id: 2]
+        3. add the [id, id] pair image corresponding [file_path] into the result array: [pair]
+        4. return the id pair
+    :param path: root data file path
+    :param set: "val" or "train" | dataset name
+    :param num_id: whole entity number
+    :param positive: "True" or "False", if is positive image pair
+    :return: a pair of image file path
+    """
     pair = []
     if positive:
         value = int(random.random() * num_id)
@@ -94,11 +109,12 @@ def get_num_id(path, set):
     get all entities number of the Dataset (:param set)
     :param path: the dataset root path
     :param set: dataset name
-    :return:
+    :return: the whole number or the length, entity number
     """
     files = os.listdir('%s/labeled/%s' % (path, set))
     files.sort()
     return int(files[-1].split('_')[0]) - int(files[0].split('_')[0]) + 1
+
 
 def read_data(path, set, num_id, image_width, image_height, batch_size):
     batch_images = []
@@ -108,11 +124,19 @@ def read_data(path, set, num_id, image_width, image_height, batch_size):
         for pair in pairs:
             images = []
             for p in pair:
+                # get image tensor, and add it to a array
                 image = cv2.imread(p)
                 image = cv2.resize(image, (image_width, image_height))
                 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
                 images.append(image)
+            # [images] : [rgb_image_tensor1, rgb_image_tensor2]
+            # get image tensors array
             batch_images.append(images)
+            # [batch_images] :
+            #   [
+            #       [positive_rgb_img_tensor_1, positive_rgb_img_tensor_2],
+            #       [negative_rgb_img_tensor_1, negative_rgb_img_tensor_2],
+            #   ]
         labels.append([1., 0.])
         labels.append([0., 1.])
 
@@ -125,6 +149,7 @@ def read_data(path, set, num_id, image_width, image_height, batch_size):
                 exit()
     '''
     return np.transpose(batch_images, (1, 0, 2, 3, 4)), np.array(labels)
+
 
 if __name__ == '__main__':
     prepare_data(sys.argv[1])
