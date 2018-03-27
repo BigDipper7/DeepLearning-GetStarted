205d4af0608ae200db0a2c1762f26e1b0e51606e
ViolinSolo <i.violinsolo@gmail.com>
2018-03-27 21:55:43 +0800

    [tensorflow] 增加了一个resnet接fc层的层

--- TF-Persion-ReID/baseline/model.py
+++ TF-Persion-ReID/baseline/model.py
@@ -4,6 +4,8 @@
 # Created on 27/03/2018]
 
 import tensorflow as tf
+# from keras import backend as K
+# from keras.applications.resnet50 import ResNet50
 
 
 class Yggdrasil:
@@ -11,16 +13,27 @@ class Yggdrasil:
     in_height = 180
     in_tunnel = 3
     batch_size = 32
+    keep_prob = 0.5
 
     def __init__(self, n_class):
         self.n_class = n_class
 
-        # X = tf.placeholder(tf.float32, shape=(None, self.in_height, self.in_width, self.in_tunnel))
-        # Y = tf.placeholder(tf.float32, shape=(None, n_class))
-
-        # tf.nn.softmax(l)
-
     def model(self, X):
 
+        resnet50 = tf.keras.applications.resnet50.ResNet50(include_top=False, weights='imagenet', input_tensor=X)
+        print(resnet50.shape)
 
-        return None
+        avg_pool = tf.nn.avg_pool(resnet50, ksize=[1, 1, 1, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+        flatten1 = tf.layers.flatten(avg_pool)  # Flatten the tensor and keep batch_size dim
+        linear1 = tf.layers.dense(flatten1, units=512, activation=None, use_bias=True)
+
+        bn1 = tf.layers.batch_normalization(linear1, axis=0)
+
+        relu1 = tf.nn.relu(bn1)
+
+        dropout1 = tf.nn.dropout(relu1, keep_prob=self.keep_prob)
+
+        logits = tf.layers.dense(dropout1, units=self.n_class, activation=None, use_bias=True)
+
+        return logits
