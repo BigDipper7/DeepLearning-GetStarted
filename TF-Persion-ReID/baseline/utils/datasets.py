a103ae934d36d28fcce35e099bb160d12821db9f
ViolinSolo <i.violinsolo@gmail.com>
2018-04-01 02:14:49 +0800

    [tensorflow] 增加了测试代码，获取测试集的数据操作，但是只需要更新下，取消-1的

--- TF-Persion-ReID/baseline/utils/datasets.py
+++ TF-Persion-ReID/baseline/utils/datasets.py
@@ -8,6 +8,7 @@ import os
 
 def get_data_list(root_pth):
     _dataset_train = {'images': [], 'labels': []}
+    _dataset_test = {'images': [], 'labels': []}
 
     train_pth = os.path.join(root_pth, "bounding_box_train")
     # val_pth = os.path.join(root_pth, "val")
@@ -25,5 +26,14 @@ def get_data_list(root_pth):
             _dataset_train['images'].append(os.path.join(root, file_name))
             _dataset_train['labels'].append(int(tmp_label))  # 强行增加了一个label的数字化的代码
 
-    return _dataset_train
+    for root, dirs, files in os.walk(test_pth, topdown=True):
+        for file_name in files:
+            if not file_name[-4:] == '.jpg':
+                continue
+            fsplits = file_name.split('_')
+            tmp_label = fsplits[0]
+            _dataset_test['images'].append(os.path.join(root, file_name))
+            _dataset_test['labels'].append(int(tmp_label))  # 强行增加了一个label的数字化的代码
+
+    return _dataset_train, _dataset_test
 
