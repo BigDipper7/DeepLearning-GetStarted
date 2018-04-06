a8ebc364aaa1c8403d1be52129cb375c53aad27f
ViolinSolo <i.violinsolo@gmail.com>
2018-04-06 16:54:02 +0800

    [tensorflow] 给model增加了层次，看起来更加有层次感

--- TF-Persion-ReID/baseline/model.py
+++ TF-Persion-ReID/baseline/model.py
@@ -24,23 +24,27 @@ class Yggdrasil:
 
     def model(self, X):
 
-        resnet50 = tf.keras.applications.resnet50.ResNet50(include_top=False, weights='imagenet', input_tensor=X, pooling=None)
-        print("==== Shape : ====")
-        print(resnet50.output_shape)
-        print("==== Shape +++ ====")
+        with tf.name_scope('network'):
+            with tf.name_scope('resnet50'):
+                resnet50 = tf.keras.applications.resnet50.ResNet50(include_top=False, weights='imagenet', input_tensor=X, pooling=None)
+                print("==== Shape : ====")
+                print(resnet50.output_shape)
+                print("==== Shape +++ ====")
 
-        # 注意的是resnet50本身不是tensor，所以需要指定输出的tensor是什么
-        avg_pool = tf.nn.avg_pool(resnet50.output, ksize=[1, 1, 1, 1], strides=[1, 2, 2, 1], padding='SAME')
+            with tf.name_scope('avg_pool1'):
+                # 注意的是resnet50本身不是tensor，所以需要指定输出的tensor是什么
+                avg_pool = tf.nn.avg_pool(resnet50.output, ksize=[1, 1, 1, 1], strides=[1, 2, 2, 1], padding='SAME')
 
-        flatten1 = tf.layers.flatten(avg_pool)  # Flatten the tensor and keep batch_size dim
-        linear1 = tf.layers.dense(flatten1, units=512, activation=None, use_bias=True)
+            with tf.name_scope('fully_connected1'):
+                flatten1 = tf.layers.flatten(avg_pool)  # Flatten the tensor and keep batch_size dim
+                linear1 = tf.layers.dense(flatten1, units=512, activation=None, use_bias=True)
 
-        bn1 = tf.layers.batch_normalization(linear1, axis=-1)
+                bn1 = tf.layers.batch_normalization(linear1, axis=-1)
 
-        relu1 = tf.nn.relu(bn1)
+                relu1 = tf.nn.relu(bn1)
 
-        dropout1 = tf.nn.dropout(relu1, keep_prob=self.keep_prob)
-
-        logits = tf.layers.dense(dropout1, units=self.n_class, activation=None, use_bias=True)
+                dropout1 = tf.nn.dropout(relu1, keep_prob=self.keep_prob)
+            with tf.name_scope('logits'):
+                logits = tf.layers.dense(dropout1, units=self.n_class, activation=None, use_bias=True)
 
         return logits
