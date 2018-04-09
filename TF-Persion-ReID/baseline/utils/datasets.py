c709dd2afe562f20103a5b2b0044d13dbf3eee46
ViolinSolo <i.violinsolo@gmail.com>
2018-04-09 14:08:41 +0800

    [tensorflow] 增加了一个对valid的数据集进行整合的数据集获取操作

--- TF-Persion-ReID/baseline/utils/datasets.py
+++ TF-Persion-ReID/baseline/utils/datasets.py
@@ -70,11 +70,32 @@ def get_val_train_ds(split_root_pth):
         # 直接结束循环，后面的循环已经没有什么意义了
         break
 
+    for root, dirs, files in os.walk(_valid_path, topdown=True):
+        print(root)
+        print(dirs)
+        print(files)
 
-    return _dataset_train
+        for str_class_name in dirs:
+            # 遍历所有的class name，即包含的dir们
+            _tmp_valid_set_pth = os.path.join(root, str_class_name)
+
+            for c_root, c_dirs, c_files in os.walk(_tmp_valid_set_pth, topdown=True):
+                for c_file in c_files:
+                    if not c_file[-4:] == '.jpg':
+                        continue
+                    # 遍历获取每次的file name，用于拼接出image的path
+                    _dataset_valid['images'].append(os.path.join(c_root, c_file))
+                    _dataset_valid['labels'].append(int(str_class_name))  # 强行增加了一个label的数字化的代码
+                # 结束当前循环，后面没意义了
+                break
+
+        # 直接结束循环，后面的循环已经没有什么意义了
+        break
+
+    return _dataset_train, _dataset_valid
 
 
 
 if __name__ == '__main__':
     from const import DS_ROOT_PTH
-    x_ds_train = get_val_train_ds(DS_ROOT_PTH)
+    x_ds_train, x_ds_valid = get_val_train_ds(DS_ROOT_PTH)
