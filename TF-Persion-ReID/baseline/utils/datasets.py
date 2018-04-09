4acea35ab601d73259ac37e11902a3766212855c
ViolinSolo <i.violinsolo@gmail.com>
2018-04-09 14:05:21 +0800

    [tensorflow] 增加了一个对已有的label进行标注的操作

--- TF-Persion-ReID/baseline/utils/datasets.py
+++ TF-Persion-ReID/baseline/utils/datasets.py
@@ -40,3 +40,41 @@ def get_data_list(root_pth):
 
     return _dataset_train, _dataset_test
 
+
+def get_val_train_ds(split_root_pth):
+    _dataset_train = {"images": [], "labels": []}
+    _dataset_valid = {"images": [], "labels": []}
+
+    _train_path = os.path.join(split_root_pth, "pytorch/train")
+    _valid_path = os.path.join(split_root_pth, "pytorch/val")
+
+    for root, dirs, files in os.walk(_train_path, topdown=True):
+        print(root)
+        print(dirs)
+        print(files)
+
+        for str_class_name in dirs:
+            # 遍历所有的class name，即包含的dir们
+            _tmp_train_set_pth = os.path.join(root, str_class_name)
+
+            for c_root, c_dirs, c_files in os.walk(_tmp_train_set_pth, topdown=True):
+                for c_file in c_files:
+                    if not c_file[-4:] == '.jpg':
+                        continue
+                    # 遍历获取每次的file name，用于拼接出image的path
+                    _dataset_train['images'].append(os.path.join(c_root, c_file))
+                    _dataset_train['labels'].append(int(str_class_name))  # 强行增加了一个label的数字化的代码
+                # 结束当前循环，后面没意义了
+                break
+
+        # 直接结束循环，后面的循环已经没有什么意义了
+        break
+
+
+    return _dataset_train
+
+
+
+if __name__ == '__main__':
+    from const import DS_ROOT_PTH
+    x_ds_train = get_val_train_ds(DS_ROOT_PTH)
